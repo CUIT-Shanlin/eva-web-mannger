7318cc199eaf26e8e5194d7927470277fe054081
JamesGosing <3248175240@qq.com>
2024-07-20 17:45:40 +0800

    feat: 完成登录页页面构造

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,11 +2,29 @@
   <div class="loginAll">
     <div class="dialogBox">
         <div class="loginDialog">
-            <h1 class="title">欢迎登录智慧评教后台</h1>
-
+            <h1 class="title">欢迎使用智慧评教后台</h1>
             <div class="iptBar">
-                1
+                <div class="iconfont ico">&#xe65e;</div>
+                <div class="iptBox">
+                    <input class="ipt" placeholder="请输入账号"/>
+                </div>
             </div>
+            <div class="iptBar">
+                <div class="iconfont ico">&#xe635;</div>
+                <div class="iptBox">
+                    <input class="ipt" placeholder="请输入密码" type="password"/>
+                </div>
+            </div>
+            <div class="checkboxLine">
+                <input type="checkbox" id="box1"/>
+                <label for="box1">记住账号</label>
+
+                <input type="checkbox" id="box2"/>
+                <label for="box2">自动登录</label>
+            </div>
+            
+            <button class="btn">立即登录</button>
+
         </div>
     </div>
     <img src="../assets/loginBack2.png" class="imgShow"/>
@@ -15,9 +33,12 @@
 
 <script setup>
 
+
+
 </script>
 
 <style lang="scss" scoped>
+@import url('../assets/font/iconfont.css');
 @import '../styles/commonFlexStyles.scss';
 
 .loginAll{
@@ -42,19 +63,102 @@
             border-radius: 20px;
             background: #EFF6FF;
             box-shadow: 0px 10px 30px #b7c1f1;
-            
             .title{
                 text-align: center;
                 margin-bottom: 40px;
             }
             .iptBar{
-                width: 93%;
+                $iptFontColor: rgb(50,78,199);
+                width: 100%;
                 height: 35px;
-                padding: 11px 11px;
+                padding: 11px 0;
+                margin-top: 30px;
+                margin-bottom: 12px;
                 border-radius: 8px;
                 background: rgb(220,232,255);
                 @include flex-center-y;
+                color: $iptFontColor;
+                .ico{
+                    font-size: 30px;
+                    padding: 0 17px;
+                }
+                .iptBox{
+                    height: 80%;
+                    padding: 0 17px;
+                    border-left: 1.5px $iptFontColor solid;
+                    .ipt{
+                        width: 100%;
+                        height: 100%;
+                        border: 0;
+                        background: rgba(255,255,255,0);
+                        font-size: 20px;
+                    }
+                    input:focus {  
+                        outline: none;
+                        border: none;
+                    }
+                    input::placeholder{
+                        color: $iptFontColor;
+                    }
+                }
             }
+            .checkboxLine{
+                margin-top: 5px;
+                float: right;
+                @include flex-center-y;
+                $boxBorderColor: rgb(100,122,213);
+                $boxBorderSize: 1.5px;
+                color: $boxBorderColor;
+                input[type=checkbox]{
+                    width: 14px;
+                    height: 14px;
+                    margin-right: 8px;
+                    margin-left: 15px;
+                    background-color: #fff;
+                    border: $boxBorderSize solid rgb(137, 157, 238);
+                    border-radius: 2px;
+                    color: #fff;
+                    text-align: center;
+                    line-height: 14px;
+                    -webkit-appearance: none;
+                    appearance: none;
+                    outline: none;
+                }
+                input[type="checkbox"]:hover {
+                    border: $boxBorderSize solid $boxBorderColor;
+                }
+                input[type=checkbox]:checked {
+                    color: #fff;
+                    background-color: $boxBorderColor;
+                    border: $boxBorderSize solid $boxBorderColor;
+                }
+                input[type=checkbox]:after {
+                    content: "\2713";
+                }
+            }
+            
+            .btn{
+                margin-top: 55px;
+                outline: none;
+                border-radius: 5px;
+                border: 0px;
+                background: rgb(78,105,244);
+                width: 100%;
+                height: 12%;
+                font-size: 20px;
+                color: #FFF;
+                transition: border-radius 2s;
+                -webkit-transition: border-radius 1s;
+                &:hover{
+                    background: rgb(109, 128, 228);
+                    border-bottom-right-radius: 50px;
+                    border-top-left-radius: 50px;
+                    border-bottom-left-radius: 10px;
+                    border-top-right-radius: 10px;
+                }
+            }
+
+
         }
     }
     .imgShow{
