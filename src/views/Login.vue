6a92bcf4cd88d2d2f2a8702c06b64dde846820ef
JamesGosing <3248175240@qq.com>
2024-07-20 15:38:33 +0800

    chore: 导入iconfont文件

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,6 +2,11 @@
   <div class="loginAll">
     <div class="dialogBox">
         <div class="loginDialog">
+            <h1 class="title">欢迎登录智慧评教后台</h1>
+
+            <div class="iptBar">
+                1
+            </div>
         </div>
     </div>
     <img src="../assets/loginBack2.png" class="imgShow"/>
@@ -29,11 +34,27 @@
         height: 100%;
         @include flex-center;
         .loginDialog{
-            width: 59.64%;
-            height: 55.6%;
+            // width: 59.64%;
+            // height: 55.6%;
+            height: 50%;
+            width: 47%;
+            padding: 7% 7%;
             border-radius: 20px;
             background: #EFF6FF;
             box-shadow: 0px 10px 30px #b7c1f1;
+            
+            .title{
+                text-align: center;
+                margin-bottom: 40px;
+            }
+            .iptBar{
+                width: 93%;
+                height: 35px;
+                padding: 11px 11px;
+                border-radius: 8px;
+                background: rgb(220,232,255);
+                @include flex-center-y;
+            }
         }
     }
     .imgShow{
