2c1402a3002facefd4a543a7ba5b355c95d9f062
JamesGosing <3248175240@qq.com>
2024-07-20 14:35:27 +0800

    feat: 完成登录页基本样式布局

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="loginAll">
-    <h1>登录666</h1>
+    <div class="dialogBox">
+        <div class="loginDialog">
+        </div>
+    </div>
+    <img src="../assets/loginBack2.png" class="imgShow"/>
   </div>
 </template>
 
@@ -9,7 +13,33 @@
 </script>
 
 <style lang="scss" scoped>
+@import '../styles/commonFlexStyles.scss';
+
 .loginAll{
-    color: rgb(30, 131, 219);
+    background-image: url("../assets/loginBack1.png");
+    height: 100%;
+    width: 100%;
+    display: flex;
+    top: 0;
+    left: 0;
+    overflow: hidden;
+    position: fixed;
+    .dialogBox{
+        width: 42%;
+        height: 100%;
+        @include flex-center;
+        .loginDialog{
+            width: 59.64%;
+            height: 55.6%;
+            border-radius: 20px;
+            background: #EFF6FF;
+            box-shadow: 0px 10px 30px #b7c1f1;
+        }
+    }
+    .imgShow{
+        width: 58.4375%;
+        position: fixed;
+        right: 0;
+    }
 }
 </style>
