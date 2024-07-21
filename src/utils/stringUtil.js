5fdf93b5a80e01e8202f88fbd8ced499886e3a59
JamesGosing <3248175240@qq.com>
2024-07-21 12:27:26 +0800

    feat: 新增了字符串工具js

---
+++ src/utils/stringUtil.js
@@ -0,0 +1,33 @@
+/**
+ * 处理字符串的工具js
+ */
+
+/**
+ * 判断是不是空白字符串或是null
+ * @param {string} str 传入字符串 
+ * @returns 是否为空白字符串
+ */
+export function isSpace(str){
+    return str == null || /^\s*$/.test(str);
+}
+
+/**
+ * 去除一个字符串中空白字符
+ * @param {string} str 传入字符串
+ * @returns 去除空白字符之后的字符串
+ */ 
+export function removeWhiteSpaces(str) {
+    // 使用正则表达式匹配所有空白字符，并用空字符串替换它们
+    return str.replace(/\s/g, '');
+}
+/**
+ * 处理日期字符串 将完整日期字符串转成MM.DD格式
+ * @param {string} dateString 日期字符串
+ * @returns 处理完之后的日期字符串
+ */
+export function choreDateStr(dateString){
+    let parts = dateString.split(' ')[0].split('-');
+    let month = parts[1];
+    let day = parts[2].trim(); // 去除字符串两端的空格
+    return `${month}.${day}`;
+}
