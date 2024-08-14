66f0b2915d9066680d65e69110fa71c43469a68e
JamesGosing <3248175240@qq.com>
2024-08-14 13:05:02 +0800

    chore: 初始化操作日志页面

---
+++ src/views/system/sysLog/list.vue
@@ -0,0 +1,14 @@
+<!-- 操作日志页面 -->
+<template>
+  <PageTitle content="操作日志列表" />
+
+</template>
+
+<script setup>
+import PageTitle from "@/components/PageTitle.vue";
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
