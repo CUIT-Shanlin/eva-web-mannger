fe2671780a27168751a6370e75ce7b4179bcdb25
JamesGosing <3248175240@qq.com>
2024-08-20 20:38:53 +0800

    feat: 初始化课程类型页面

--- src/api/course.js
+++ src/api/course.js
@@ -9,3 +9,18 @@ export function getAllBaseCourse(){
         url: '/courses/all'
     })
 }
+
+/**
+ * 分页获取课程类型信息
+ * @param {Number} page 页数
+ * @param {Number} size 一页的元素个数
+ * @param {Object} queryObj 条件查询的对象
+ * @returns 分页信息
+ */
+export function getPageData(reqData = {page: 0,size: 0,queryObj: {}}){
+    return request({
+        url: '/course/types',
+        method: 'POST',
+        data: reqData
+    })
+}
