b3e054b409b5c5cd76cf03b44490e12ec9e5d876
JamesGosing <3248175240@qq.com>
2024-09-08 16:47:14 +0800

    chore: 初始化评教看板页面

---
+++ src/views/dashboard/evaBoard.vue
@@ -0,0 +1,14 @@
+<!-- 评教看板页面 -->
+<template>
+  <PageTitle content="评教看板" />
+  
+</template>
+
+<script setup>
+import PageTitle from "@/components/PageTitle.vue";
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
