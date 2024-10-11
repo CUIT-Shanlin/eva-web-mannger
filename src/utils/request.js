aa6bebffbf4c04770ead6e514e715559d6e4feb9
JamesGosing <3248175240@qq.com>
2024-10-11 21:38:22 +0800

    refactor: 优化了axios的二次封装，增加了权限判断

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,12 @@
 import axios from 'axios'
-import { getMyToken } from './auth';
-import { useWarningConfirm,useFailedTip } from './msgTip';
+import {
+  getMyToken,
+  removeToken
+} from './auth';
+import { 
+  useWarningConfirm,
+  useFailedTip
+} from './msgTip';
 import router from '@/router/permissions'
 
 /**
@@ -42,19 +48,28 @@ request.interceptors.request.use(
 request.interceptors.response.use(
     response=>{
         let {data} = response
-        if(data.code === 200){
+        if(data.code + '' === '200'){
             // useSuccessTip(data.msg)
             return Promise.resolve(data.data)
         }else{
             // useSuccessTip(data.msg)
-            useFailedTip(data.msg)
+            if(data.code + '' === '401'){ // authorized，token过期或token异常等的返回码
+                useWarningConfirm('登录过期或异常，即将跳转登录页，重新登录').then(()=>{
+                    removeToken()
+                    window.location.reload()
+                })
+            }else if(data.code + '' === '403'){ // 没有权限的情况
+                useFailedTip("您没有权限进行该操作！")
+            }else{
+                useFailedTip(data.msg)
+            }
             return Promise.reject(data.data)
         }
     },
     error=>{
         useFailedTip('响应错误：' + error.message)
         if(!getMyToken()){
-            useWarningConfirm('登录失效，即将跳转登录页，重新登录').then(()=>{
+            useWarningConfirm('登录异常，即将跳转登录页，重新登录').then(()=>{
                 // router.push('/login')
                 window.location.reload()
             })
