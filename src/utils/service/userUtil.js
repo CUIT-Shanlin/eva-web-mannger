d4f8d25a95aeab34ee6c1a8913618cb1746f83ba
JamesGosing <3248175240@qq.com>
2024-10-18 20:17:57 +0800

    refactor: 移除旧版头像处理方案

--- src/utils/service/userUtil.js
+++ src/utils/service/userUtil.js
@@ -1,38 +1,10 @@
 /**
  * 处理用户模块业务相关的工具js
  */
-
-import { getUserAvatar } from "@/api/user";
-import { isSpace } from "@/utils/stringUtil"
 import {
     EVA_UNQUALIFIED_USER,
     UNQUALIFIED_USER,
 }from '@/utils/service/staticData'
-
-/**
- * 获取 头像url
- * @param {String|Number} userId 用户id 
- * @returns 头像url
- */
-export async function getMyAvatar(userId = -1){
-    // 默认头像地址
-    const DEFAULT_AVATAR_URL2 = '/src/assets/img/default_avatar.png'
-    try{
-        let res = await getUserAvatar(userId)
-        // if(!res instanceof Blob){
-        //     return DEFAULT_AVATAR_URL;
-        // }
-        const avatar = URL.createObjectURL(res);
-        if(isSpace(avatar) || res.status + '' === '404'){
-            return DEFAULT_AVATAR_URL2;
-        }
-        return avatar
-    }catch{
-        return DEFAULT_AVATAR_URL2;
-    }
-}
-
-export const DEFAULT_AVATAR_URL2 = '/src/assets/img/default_avatar.png'
 
 
 // TODO 操作评教和被评教的达标要求设置
