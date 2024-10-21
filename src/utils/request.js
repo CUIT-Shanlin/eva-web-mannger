91725541c407ee631ff6a359fdf448dff309b591
JamesGosing <3248175240@qq.com>
2024-10-21 23:22:15 +0800

    chore: 完成测试等环境的配置

--- src/utils/request.js
+++ src/utils/request.js
@@ -16,7 +16,8 @@ import router from '@/router/permissions'
 axios.defaults.withCredentials = true;
 
 const request = axios.create({
-    baseURL: '/devApi'
+    // baseURL: '/devApi'
+    baseURL: import.meta.env.VITE_APP_BASE_URL
 })
 
 // 添加请求拦截-token处理
--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,40 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    host: "localhost",
-    port: 5666,
-    proxy: {
-      "/devApi": {
-        // target: "http://localhost:4523", // 后端地址
-        target: "https://apifoxmock.com/m1/4684209-4335706-default", // 后端地址
-        changeOrigin: true,            // 是否换源
-        rewrite: (path) => path.replace(/^\/devApi/, "")
+export default defineConfig(({command, mode})=>{
+  const env = loadEnv(mode, './');
+  return {
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
       }
-    }
-  },
+    },
+    server: {
+      host: "localhost",
+      port: 5666,
+      proxy: {
+        "/api": {
+          // target: "https://apifoxmock.com/m1/4684209-4335706-default",
+          target: env.VITE_APP_BASE_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, "")
+        }
+      }
+    },
 
-  css: {
-    // css预处理器
-    preprocessorOptions: {
-      scss: {
-        // additionalData: '@import "@/assets/style/mixin.scss";'
+    css: {
+      // css预处理器
+      preprocessorOptions: {
+        scss: {
+          // additionalData: '@import "@/assets/style/mixin.scss";'
+        }
       }
     }
   }
-
 })
