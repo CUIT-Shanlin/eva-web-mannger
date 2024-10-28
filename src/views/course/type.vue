7f5a605c4e8529497483958be34af1b797e1188a
JamesGosing <3248175240@qq.com>
2024-10-28 23:03:40 +0800

    fix: 解决了课程类型删除失败的问题

--- src/views/course/type.vue
+++ src/views/course/type.vue
@@ -169,7 +169,6 @@ import {
 import { isEmptyArr, deepCopy } from "@/utils/objUtil";
 import { removeSpace } from "@/utils/stringUtil";
 import { hasBtnPermission } from '@/utils/btnPermission';
-import { useRouter } from "vue-router";
 
 const isLoadingBtn = ref(false)
 
@@ -270,7 +269,7 @@ function removeOneType(type) {
   }
   useSimpleConfirm(`你确定要删除课程类型 “${type.name}” 吗？`).then(
     async () => {
-      let res = await removeOne(type);
+      await removeOne(type.id);
       useSuccessTip(`成功删除课程类型 “${type.name}”`);
       getMyPageData()
     }
