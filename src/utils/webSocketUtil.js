9d5a17f69fd669371d5e0b93c9377ff9d57921a7
Lystran <1934944134@qq.com>
2025-12-19 15:55:14 +0800

    chore: 更新后端端口

--- src/utils/webSocketUtil.js
+++ src/utils/webSocketUtil.js
@@ -6,7 +6,7 @@ import { getToken } from "@/utils/auth";
 
 
 // 后端socket的地址
-const HOST = import.meta.env.VITE_APP_MODE === 'development' ? 'apifoxmock.com/m1/4684209-4335706-default:8080' : 'eva-match.project.littlestrange.site/api'
+const HOST = import.meta.env.VITE_APP_MODE === 'development' ? 'apifoxmock.com/m1/4684209-4335706-default:8080' : 'eva-match.project.littlestrange.site:52443/api'
 const WSS_ADDRESS = `wss://${HOST}/send/msg`
 
 /**
