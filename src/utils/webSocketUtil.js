1ccc3587009a76e57d09dda0567c3220768b3e0e
LystranG <me@lystran.com>
2026-02-21 13:38:13 +0800

    chore: update domain

--- src/utils/webSocketUtil.js
+++ src/utils/webSocketUtil.js
@@ -9,7 +9,7 @@ import { getToken } from "@/utils/auth";
 const HOST =
   import.meta.env.VITE_APP_MODE === "development"
     ? "apifoxmock.com/m1/4684209-4335706-default:8080"
-    : "eva.project.littlestrange.site:52443/api";
+    : "eva.lystran.com:52443/api";
 const WSS_ADDRESS = `wss://${HOST}/send/msg`;
 
 /**
